0654dccd9adb038bb372c05c6a7a7117d14919aa
MuhmmadAbubakar <abubakarmughal1401@gmail.com>
2025-03-12 10:24:41 -0700

    Reorganized project structure into packages

--- src/com/clinic/Patient.java
+++
@@ -1,7 +0,0 @@
-package com.clinic.entities;
-
-public class Patient extends Person {
-    public Patient(String id, String fullName, String address, String phoneNumber) {
-        super(id, fullName, address, phoneNumber);
-    }
-}
--- src/com/clinic/main.java
+++ src/com/clinic/main.java
@@ -11,7 +11,8 @@ public class Main {
         Clinic clinic = new Clinic();
 
         Patient patient1 = new Patient("P001", "John Doe", "123 Street", "1234567890");
-        Physiotherapist physio1 = new Physiotherapist("T001", "Dr. Smith", "456 Avenue", "0987654321", new String[]{"Physiotherapy"});
+        Physiotherapist physio1 = new Physiotherapist("T001", "Dr. Smith", "456 Avenue", "0987654321",
+                new String[] { "Physiotherapy" });
         Treatment treatment1 = new Treatment("Massage", 60);
         Appointment appointment1 = new Appointment("A001", "1st May 2025, 10:00", patient1, physio1, treatment1);
 
